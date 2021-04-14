eafdc44800d7d1a82a3bc5f4b9919f92824b5616
test <zozo@DESKTOP-BE29PVD.localdomain>
2021-04-14 19:48:18 +0200

    bzero norme ok

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,16 +1,15 @@
 #include "libft.h"
 
-void    ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-    size_t i;
-    unsigned char *d;
-    
-    i = 0;
-    d = (unsigned char *)s;
-    while (i < n)
-    {
-        d[i] = 0;
-        i++;
-    }
-    s = d;
+    size_t	i;
+    unsigned char	*d;
+	i = 0;
+	d = (unsigned char *)s;
+	while (i < n)
+	{
+		d[i] = 0;
+		i++;
+	}
+	s = d;
 }
