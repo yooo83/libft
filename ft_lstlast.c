4a467df8f06264e969c98ddc69cd825f69ca6ba4
test <zozo@DESKTOP-BE29PVD.localdomain>
2021-04-14 16:21:21 +0200

    lstlast modified

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -2,11 +2,8 @@
 
 t_list *ft_lstlast(t_list *lst)
 {
-	if(lst == NULL)
-		exit(EXIT_FAILURE);
-	while(lst->next)
-	{
-		lst = lst->next;
-	}
+	if(lst)
+		while(lst->next)
+			lst = lst->next;
 	return (lst);
 }
