5e5498480280c2b7f27ec7a3c2b8f8c2f810a618
test <zozo@DESKTOP-BE29PVD.localdomain>
2021-04-14 19:34:37 +0200

    atoi norme ok

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,8 +3,8 @@
 int		ft_atoi(const char *str)
 {
 	size_t	i;
-	size_t num;
-	size_t negatif;
+	size_t	num;
+	size_t	negatif;
 
 	i = 0;
 	negatif = 1;
@@ -19,7 +19,7 @@ int		ft_atoi(const char *str)
 		}
 		i++;
 	}
-	while(str[i] <= '9' && str[i] >= '0')
+	while (str[i] <= '9' && str[i] >= '0')
 	{
 		num = num * 10 + (str[i] - 48);
 		i++;
