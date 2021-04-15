ccae8da28144b987a400055d1eba6d523642e9e1
test <zozo@DESKTOP-BE29PVD.localdomain>
2021-04-15 15:49:42 +0200

    Header + norme ok

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygaiero <yoanngaiero@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 15:46:06 by ygaiero           #+#    #+#             */
+/*   Updated: 2021/04/15 15:49:13 by ygaiero          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
@@ -15,7 +27,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			{
 				ft_lstclear(&new, del);
 				return (NULL);
-			}	
+			}
 			ft_lstadd_back(&new, new2);
 			lst = lst->next;
 		}
